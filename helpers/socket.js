fe2e4ec5eb46c8358e8c6e53b9e1ae1a4652fa23
Thanhnd <thanhnd@visionnetwork.vn>
2023-03-01 17:47:48 +0700

    update socket event

--- helpers/socket.js
+++ helpers/socket.js
@@ -1,41 +1,48 @@
 let users = [];
 
 const addUser = (client, socketId) => {
-    !users.some(user => user.userId === client._id) &&
-        users.push({ ...client, socketId });
-}
+  !users.some((user) => user.userId === client._id) &&
+    users.push({ ...client, socketId });
+};
 
 const removeUser = (socketId) => {
-    users = users.filter(user => user.socketId !== socketId);
-}
+  users = users.filter((user) => user.socketId !== socketId);
+};
 
 const getSingleUser = (userId) => {
-    receiverUser = users.find(user => user._id === userId);
-    return receiverUser
-}
+  receiverUser = users.find((user) => user._id === userId);
+  return receiverUser;
+};
 
 const socketActions = (socket, io) => {
-    //When user conect
-    console.log('A user connected');
+  //When user conect
+  console.log("A user connected");
 
-    //Take userId and socketId from user
-    socket.on("addUser", user => {
-        addUser(user, socket.id);
-        io.emit("getUsers", users)
-    })
+  //Take userId and socketId from user
+  socket.on("addUser", (user) => {
+    addUser(user, socket.id);
+    io.emit("getUsers", users);
+  });
 
-    //Send and get message
-    socket.on("sendMessage", ({ receiverId, ...data }) => {
-        const user = getSingleUser(receiverId);
-        io.to(user.socketId).emit("getMessage", data)
-    })
+  //Send and get message
+  socket.on("sendMessage", ({ receiverId, ...data }) => {
+    const user = getSingleUser(receiverId);
+    io.to(user.socketId).emit("getMessage", data);
+  });
 
-    //when disconnect
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-        removeUser(socket.id);
-        io.emit("getUsers", users);
-    })
-}
+  //when disconnect
+  socket.on("userLogout", () => {
+    console.log("A user logout");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 
-module.exports = { socketActions }
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+};
+
+module.exports = { socketActions };
